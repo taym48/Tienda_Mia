dc2cf24a144ca7b78f6d702f07559c6a779c1282
taym48 <tayron.murillo10@gmail.com>
2025-03-13 18:12:33 -0600

    Updated

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria; 
+import java.util.List;
+
+public interface CategoriaService {
+
+   //Se obtiene listado de la BD
+    public List<Categoria> getCategorias(boolean activos);
+    
+}
